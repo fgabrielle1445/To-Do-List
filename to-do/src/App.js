23cd44a1d339d38e8ffdc3f1aa80fdd4ce634f08
Gabby <fgabrielle1445@yahoo.com>
2021-06-15 00:30:41 -0600

    Updated to-do UI style

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
 
 function App() {
+  // Style for the "Add to list" button
+  const btn_style = {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="Header">
+      <h1>To-Do List</h1>
+      </div>
+      <form noValidate autoComplete="off">
+        <TextField id="outlined-basic" label="Enter Task" variant="outlined" />
+      </form>
+
+      <Button style={btn_style} onClick={() => { alert('clicked') }}>Add to list</Button>
+
     </div>
   );
 }
